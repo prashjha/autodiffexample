01059661b874dd470f2f44f6e7555854c6143788
David Fuentes <fuentesdt@gmail.com>
2023-04-12 11:53:32 -0500

    WIP:

--- famap/miVSvariability.m
+++ famap/miVSvariability.m
@@ -3,4 +3,24 @@
 pre1img = load('20200482-006-Pre-1.mat')
 pre2img = load('20200482-006-Pre-2.mat')
 
+midata02 = load('brutesearchNG5Nu3constDirectTotalSignalSNR02Hermite.mat')
+
+maskdata = niftiread('isofa.nii');
+
 plot( pre1img.kpla(:), pre2img.kpla(:),'x' )
+
+getMIdata = zeros(size(pyruvateFA));
+for iii =1:length(pyruvateFA(:)  ) 
+    faindex = find( midata02.pyrgrid(:)== round(pyruvateFA(iii))  & midata02.lacgrid(:)== round(lactateFA(iii)));
+    if(~isempty(faindex) )
+        getMIdata(iii) = midata02.brutesearch(faindex);
+    end
+end
+
+splitdataMI = {}
+splitdatakpl = {}
+for iii = 1:12
+    myindex =  find(maskdata(:) ==iii);
+    splitdataMI{iii} = [getMIdata(myindex );getMIdata(myindex )];
+    splitdatakpl{iii}= [pre1img.kpla(myindex );pre2img.kpla(myindex )];
+end
