92c10d2c482f8cb685277313cb8188118ea7b79a
David Fuentes <fuentesdt@gmail.com>
2023-04-12 19:35:48 -0500

    WIP:

--- famap/miVSvariability.m
+++ famap/miVSvariability.m
@@ -3,11 +3,10 @@
 pre1img = load('20200482-006-Pre-1.mat')
 pre2img = load('20200482-006-Pre-2.mat')
 
-midata02 = load('brutesearchNG5Nu3constDirectTotalSignalSNR02Hermite.mat')
-
 maskdata = niftiread('isofa.nii');
-
 plot( pre1img.kpla(:), pre2img.kpla(:),'x' )
+
+midata02 = load('brutesearchNG5Nu3constDirectTotalSignalSNR20Hermite.mat')
 
 getMIdata = zeros(size(pyruvateFA));
 for iii =1:length(pyruvateFA(:)  ) 
@@ -19,8 +18,17 @@ end
 
 splitdataMI = {}
 splitdatakpl = {}
+mivalues= nan(12,1)
+varvalues= nan(12,1)
 for iii = 1:12
-    myindex =  find(maskdata(:) ==iii);
-    splitdataMI{iii} = [getMIdata(myindex );getMIdata(myindex )];
-    splitdatakpl{iii}= [pre1img.kpla(myindex );pre2img.kpla(myindex )];
+    myindexone =  find(maskdata(:) ==iii & pre1img.masks(:)==1 );
+    myindextwo =  find(maskdata(:) ==iii & pre2img.masks(:)==1 );
+    splitdataMI{iii} = [getMIdata(myindexone );getMIdata(myindextwo )];
+    splitdatakpl{iii}= [pre1img.kpla(myindexone );pre2img.kpla(myindextwo )];
+    if(~isempty(splitdataMI{iii} ))
+      mivalues(iii)=  mean(splitdataMI{iii});
+      varvalues(iii)=  mean(splitdatakpl{iii});
+    end
 end
+plot(mivalues,varvalues,'x')
+
